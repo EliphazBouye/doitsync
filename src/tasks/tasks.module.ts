45c919bca0fa1ad6350d8c4fcefec3e1d9997ea1
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-09-23 15:57:25 +0000

    chore(): remove unused import prisma service on tasks module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   controllers: [TasksController],
