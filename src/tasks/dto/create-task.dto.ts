3af258668166a374d41b25f96e24e8dd4d0807bd
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-09-23 10:50:15 +0000

    core(): validation data task input from dto

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,13 @@
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsBoolean()
   done: boolean;
 }
