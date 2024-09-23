c93b21f0fc05f1c89b08fc3b9815288d2f6b4e4e
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-09-23 00:43:21 +0000

    core(): add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), TasksModule, DatabaseModule],
 })
 export class AppModule {}
