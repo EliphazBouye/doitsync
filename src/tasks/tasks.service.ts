a31cae2ca8a3c0bd08be5fdd4a7d45dd19e9a172
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-09-23 10:29:42 +0000

    core(): add exception for to have good errors handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,38 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Task } from './interfaces/tasks.interface';
 import { PrismaService } from 'src/database/prisma.service';
 import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   async findAll(): Promise<Task[]> {
     return await this.prisma.task.findMany();
   }
 
   async findOne(taskWhereUniqueInput: Prisma.TaskWhereUniqueInput): Promise<Task> {
-    return await this.prisma.task.findUnique({ where: taskWhereUniqueInput });
+    try {
+      return await this.prisma.task.findUniqueOrThrow({ where: taskWhereUniqueInput });
+    } catch (error) {
+      console.log(error)
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new NotFoundException(error.message);
+        }
+      }
+      throw error;
+    }
   }
 
   async create(data: Prisma.TaskCreateInput): Promise<Task> {
-    return await this.prisma.task.create({ data }); 
+    try {
+      return await this.prisma.task.create({ data });
+    } catch (error) {
+      console.error(error)
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          if (error.meta.target instanceof Array) {
+            error.meta.target.forEach(t => {
+              throw new BadRequestException(`There is a unique constraint violation, a new user cannot be created with this ${t}`);
+            })
+          }
+        }
+      }
+      throw error;
+    }
   }
 
   async update(params: {
     where: Prisma.TaskWhereUniqueInput,
     data: Prisma.TaskUpdateInput
   }): Promise<Task> {
-    const { where, data } = params;
-    return await this.prisma.task.update({
-      where,
-      data,
-    });
+    try {
+      const { where, data } = params;
+      return await this.prisma.task.update({
+        where,
+        data,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        console.log(error)
+        if (error.code === 'P2025') {
+          throw new BadRequestException(error.meta?.cause);
+        }
+        throw error;
+      }
+    }
   }
 
-  delete(where: Prisma.TaskWhereUniqueInput) {
-    return this.prisma.task.delete({
-      where
-    });
+  async delete(where: Prisma.TaskWhereUniqueInput) {
+    try {
+      await this.prisma.task.delete({
+        where
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2025') {
+          throw new BadRequestException(error.meta?.cause);
+        }
+      }
+      throw error;
+    }
   }
 }
