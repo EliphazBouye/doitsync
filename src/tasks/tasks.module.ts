c30debdfa37c73627ad40d9a89a8f3ed8fa28f01
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-09-23 15:55:55 +0000

    core(): export task service provider

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
+  exports: [TasksService],
 })
-export class TasksModule {}
+export class TasksModule { }
